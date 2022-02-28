205036c529017a238d7e5a6138a01f24af563436
Jens <jens.jens@live.dk>
2022-02-28 18:37:11 +0100

    feat(waypoint-nav): trying to correct for large error

--- src/husky_controllers/src/waypoint_navigation.cpp
+++ src/husky_controllers/src/waypoint_navigation.cpp
@@ -12,8 +12,8 @@
 
 #include "husky_controllers/utility.h"
 
-#define KP 0.5
-#define KA 0.3
+#define KP 0.2
+#define KA 0.1
 #define KB 0.01
 #define POS_TOLERANCE 0.2
 #define ANGLE_TOLERANCE 0.025
@@ -21,8 +21,9 @@
 using namespace std;
 
 auto goal = make_pair(0, 0);
-
 pair<float, float> waypoints[] = {make_pair(11.5, 0.5), make_pair(11.5, 5.5), make_pair(4, 5.5), make_pair(0, 0)};
+
+auto driving = false;
 
 ros::Publisher pub;
 ros::Subscriber sub;
@@ -52,25 +53,42 @@ auto main(int argc, char** argv) -> int {
 
         // Desired heading
         auto theta = tf::getYaw(msg->pose.pose.orientation);            // vehicle yaw
-        auto beta = -atan2(goal.second - curr_y, goal.first - curr_x);  // desired heading
+        auto beta = atan2(goal.second - curr_y, goal.first - curr_x);   // desired heading
 
+        // Convert theta and beta to radians [0 ; 2pi]
+        // theta = theta >= 0 ? theta : 2 * M_PI + theta;
+        // beta = beta >= 0 ? beta : 2 * M_PI + beta;
+
+        auto alpha = beta - theta;
         // Heading error
-        auto alpha = -beta - theta;
+        if (theta < 0 and beta >= 0) {
+            alpha = 2 * M_PI - alpha;
+        }
+        else if (theta >= 0 and beta < 0) {
+            alpha = 2 * M_PI + alpha;
+        }
+        // alpha = alpha > M_PI ? M_PI - alpha : alpha;
 
         auto command = geometry_msgs::Twist();
 
         auto v = KP * rho;
         auto omega = KA * alpha; //+ KB * beta;
 
-        if (alpha > ANGLE_TOLERANCE) {
+        ROS_WARN("state: ");
+        if (alpha > ANGLE_TOLERANCE && !driving) {
+            ROS_WARN("  turning");
             command.linear.x = 0;
             command.angular.z = omega;
         }
         else if (rho > POS_TOLERANCE) {
+            ROS_WARN("  driving");
+            driving = true;
             command.linear.x = v;
-            command.angular.z = 0;
+            command.angular.z = omega;
         }
         else {
+            ROS_WARN("  stopping");
+            driving = false;
             waypoint_index++;
             command.linear.x = 0;
             command.angular.z = 0;
@@ -89,7 +107,7 @@ auto main(int argc, char** argv) -> int {
         pub.publish(command);
     };
 
-    sub = nh.subscribe<nav_msgs::Odometry>("/husky_velocity_controller/odom", 10, odom_cb);
+    sub = nh.subscribe<nav_msgs::Odometry>("/odometry/filtered", 10, odom_cb);
 
     while(ros::ok()) {
         ros::spinOnce();
