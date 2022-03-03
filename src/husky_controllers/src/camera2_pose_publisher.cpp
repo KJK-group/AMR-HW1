b23ce4302b6ec6080dd1365adc3b592562eb2809
Kevork Christian Dounato <kevorkchristian@gmail.com>
2022-03-03 12:23:00 +0100

    feat: continuation of exercise 2.5

--- src/husky_controllers/src/camera2_pose_publisher.cpp
+++ src/husky_controllers/src/camera2_pose_publisher.cpp
@@ -1,19 +1,68 @@
 #include <ros/ros.h>
 #include <nav_msgs/Odometry.h>
-#include <tf2/tf2.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <geometry_msgs/TransformStamped.h>
+
 
 ros::Publisher camera2_pose_pub;
 ros::Subscriber odometry_filtered_sub;
 
-auto odometry_filtered_cb(const nav_msgs::Odometry::ConstPtr& msg) -> void {
+tf2_ros::Buffer* tfBuffer;
+//tf2_ros::TransformListener *tfListener;
 
+auto odometry_filtered_cb(const nav_msgs::Odometry::ConstPtr& msg) -> void {
+    geometry_msgs::TransformStamped transformStamped;
+    try{
+        transformStamped = tfBuffer->lookupTransform("camera_2_link", "base_link", ros::Time(0));
+    }
+    catch (tf2::TransformException &ex) {
+      ROS_WARN("%s",ex.what());
+      ros::Duration(1.0).sleep();
+    }
+
+    auto camera_2_odometry = nav_msgs::Odometry();
+    // Transform
+    camera_2_odometry.pose.pose.position.x = msg->pose.pose.position.x + transformStamped.transform.translation.x;
+    camera_2_odometry.pose.pose.position.y = msg->pose.pose.position.y + transformStamped.transform.translation.y;
+    camera_2_odometry.pose.pose.position.z = msg->pose.pose.position.z + transformStamped.transform.translation.z;
+
+    // Orientation / rotation
+    auto q_origin = tf2::Quaternion(
+        msg->pose.pose.orientation.x, 
+        msg->pose.pose.orientation.y,
+        msg->pose.pose.orientation.z,
+        msg->pose.pose.orientation.w
+    );
+    //tf2::fromMsg(msg->pose.pose.orientation, q_origin);
+
+    auto q_rot = tf2::Quaternion(
+        transformStamped.transform.rotation.x,
+        transformStamped.transform.rotation.y,
+        transformStamped.transform.rotation.z,
+        transformStamped.transform.rotation.w
+    );
+    //tf2::fromMsg(transformStamped.transform.rotation, q_rot);
+
+    auto q_new = q_rot*q_origin;  // Calculate the new orientation / http://wiki.ros.org/tf2/Tutorials/Quaternions
+    q_new.normalize();
+
+    //tf2::convert(q_new, camera_2_odometry.pose.pose.orientation);
+    camera_2_odometry.pose.pose.orientation.x = q_new.x();
+    camera_2_odometry.pose.pose.orientation.y = q_new.y();
+    camera_2_odometry.pose.pose.orientation.z = q_new.z();
+    camera_2_odometry.pose.pose.orientation.w = q_new.w();
+
+    camera2_pose_pub.publish(camera_2_odometry.pose.pose.orientation);
 }
 
 auto main(int argc, char** argv) -> int {
     ros::init(argc, argv, "camera2_pose_publisher");
 
     auto nh = ros::NodeHandle("~");
-    odometry_filtered_previous_time = ros::Time::now();
+    tf2_ros::Buffer _tfBuffer;
+    tf2_ros::TransformListener _tfListener(_tfBuffer);
+    tfBuffer = &_tfBuffer;
 
     camera2_pose_pub = nh.advertise<nav_msgs::Odometry>("/husky_controllers/camera2/pose", 10);
 
